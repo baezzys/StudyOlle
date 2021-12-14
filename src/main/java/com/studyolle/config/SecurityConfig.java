84fe3ad8afd5882ccd28d42cd2c74fb540ff6c82
jinwoo-Bae <wlsdn3578@gmail.com>
2021-12-14 21:50:54 +0900

    Sign-up and send email to verify user

--- src/main/java/com/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.studyolle.config;
 
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,9 +14,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    /*
+    기본적으로 스프링에서는 CSRF가 활성화 되어 있다. CSRF토큰이라는 것을 사용하여 타사이트에서 요청보내는 것을 막는다.
+    타임리프같은 폼으로 만들었을때 타임리프에서 CSRF토큰 값이 자동으로 설정되어 있다.
+    그런데 CSRF토큰 없이 폼데이터가 온다면 403 에러가 나오게 된다.
+    그래서 테스트 코드에서 에러가 나온다.
+
+
+     */
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-        .mvcMatchers("/", "/login", "sign-up", "check-email", "check-email-token", "/email-login", "/check-email-login", "login-link").permitAll()
-                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll().anyRequest().authenticated();
+                .mvcMatchers("/", "/sign-up", "/check-email-token",
+                        "/email-login", "/login-by-email", "/search/study").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
--- src/test/java/com/studyolle/account/AccountControllerTest.java
+++ src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -1,25 +1,61 @@
 package com.studyolle.account;
 
 
+import com.studyolle.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.persistence.AssociationOverride;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AccountControllerTest {
     @Autowired private MockMvc mockMvc;
 
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @MockBean
+    JavaMailSender javaMailSender;
+
     @DisplayName("회원 가입 화면 보이는지 테스트")
     @Test
     void sigUpForm() throws Exception {
-        mockMvc.perform(get("/sign-up")).andExpect(status().isOk()).andExpect(view().name("account/sign-up"));
+        mockMvc.perform(get("/sign-up")).andExpect(status().isOk()).andExpect(view().name("account/sign-up")).andExpect(model().attributeExists("signUpForm"));
+    }
+
+    @DisplayName("회원 가입 처리 - 입력값 오류")
+    @Test
+    void signUpSubmit_with_correct_input() throws Exception {
+        mockMvc.perform(post("/sign-up")
+                .param("nickname", "keesun")
+                .param("email", "wlsdn3578@gmail.com")
+                .param("password", "12345678")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+
+        Account account = accountRepository.findByEmail("wlsdn3578@gmail.com");
+        assertNotNull(account);
+        assertNotNull(account.getEmailCheckToken());
+        assertNotEquals(account.getPassword(), "12345678");
+
+        assertTrue(accountRepository.existsByEmail("wlsdn3578@gmail.com"));
+        then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
